4b171852db717329c69f7a69a2ba43cfcd9f4f59
mirabilos <t.glaser@tarent.de>
2021-12-13 20:56:26 +0100

    update the other dependencies

--- src/dist/example.sh
+++ src/dist/example.sh
@@ -6,5 +6,5 @@ exec java \
     -Djdbc.password="${dbpass:-P68ntEvJQbhI}" \
     -jar extract-tool-*-cli.jar \
     -c /dev/null \
-    -J ~/.m2/repository/org/postgresql/postgresql/42.2.14/postgresql-42.2.14.jar \
+    -J ~/.m2/repository/org/postgresql/postgresql/42.3.1/postgresql-42.3.1.jar \
     "${1:-$(dirname "$0")/example.jsn}"
