2603db6cca29696a7b24ca28b07b357c99f51154
mirabilos <t.glaser@tarent.de>
2022-02-07 21:51:55 +0100

    add workaround to make it run on JRE 17 and quell a JRE 11 warning
    cf. https://stackoverflow.com/a/70965030/2171120
    
    mid-term, this needs to move to Spring 5 or 6 even

--- src/dist/example.sh
+++ src/dist/example.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
-exec java \
+
+java=java
+
+isjava11() {
+	$java "$@" --source 11 /dev/stdin >/dev/null 2>&1 <<'	EOF'
+		class x {
+			public static void main(String[] args) {
+				System.exit(0);
+			}
+		}
+	EOF
+}
+
+j11='--add-opens java.base/java.lang=ALL-UNNAMED'
+
+if isjava11 $j11; then
+	java="$java $j11"
+fi
+
+exec $java \
     -Djdbc.driver="${db:-org.postgresql.Driver}" \
     -Djdbc.url="${dburl:-jdbc:postgresql://localhost:5432/hellophpworld}" \
     -Djdbc.username="${dbuser:-hellophpworld}" \
